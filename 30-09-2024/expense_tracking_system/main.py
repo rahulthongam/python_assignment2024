6111943330614dc7c574c33fe53f2041432d7f43
rahulthongam <106368286+rahulthongam@users.noreply.github.com>
2024-11-06 09:10:02 +0530

    Add files via upload

---
+++ 30-09-2024/expense_tracking_system/main.py
@@ -0,0 +1,22 @@
+
+import tkinder package
+widgets :
+frame
+label
+label frame
+button
+check button
+radio button
+entry
+text
+message
+checkbox
+listbox
+scrollbar
+scale
+cambus 
+menu
+
+
+from
+
